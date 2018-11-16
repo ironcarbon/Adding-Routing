6a513dfca43dee7924bd838eee4ee23c373612a8
ipek <ipekcivicakan@gmail.com>
2018-11-16 08:43:15 -0800

    Redirecting request,Conditional redirect, using history prop to redirect

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import classes from './Blog.css';
 import Posts from '../../containers/Blog/Posts/Posts';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 //import Post from '../../components/Post/Post';
 //import axios from 'axios';
 
@@ -33,6 +33,8 @@ class Blog extends Component{
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
                     <Route path="/posts" component={Posts} />
+                    <Redirect from ="/" to="/posts" />
+                    {/*<Route path="/" component={Posts} /> */}
                 </Switch>
                 
 
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import classes from './NewPost.css';
 import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 
 class NewPost extends Component{
     state={
         title:'',
         content:'',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     }
     
     componentDidMount(){
@@ -20,17 +22,24 @@ class NewPost extends Component{
         };
         axios.post('https://jsonplaceholder.typicode.com/posts', data)
         .then(response => {
-            console.log(data);
+            //console.log(data);
             console.log(response);
+            //this.setState({submitted: true})
+            this.props.history.replace('/posts');
             
         })
     }
     
 
     render(){
+        let redirect = null;
+        if(this.state.submitted) {
+            redirect= <Redirect to="/posts" />;
+        }
         return(
         
             <div className={classes.NewPost}>
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})}></input>
